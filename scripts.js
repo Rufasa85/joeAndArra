6a9fbecb6f7a0b0aa4c4d4d9194573ca0c40a024
Joe Rehfuss <joe.rehfuss@gmail.com>
2018-07-19 09:41:32 -0700

    countdown to the date and mailtolink

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-	console.log('linked');
+	const wedding = new Date('August 15 2020');
+	const now = new Date();
+	let daysLeft = (wedding-now)/1000/60/60/24;
+	$('#daysLeft').html(Math.ceil(daysLeft));
 })
