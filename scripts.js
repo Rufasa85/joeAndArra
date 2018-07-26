f45e45d1d896cf3723686ce16bae65454419f964
Joe Rehfuss <joe.rehfuss@gmail.com>
2018-07-26 15:14:35 -0700

    flexing directions

--- scripts.js
+++ scripts.js
@@ -3,4 +3,17 @@ $(document).ready(function() {
 	const now = new Date();
 	let daysLeft = (wedding-now)/1000/60/60/24;
 	$('#daysLeft').html(Math.ceil(daysLeft));
+	const directionCards = document.querySelectorAll('.directionCard')
+	console.log(directionCards);
+	function toggleOpen() {
+      this.classList.toggle('open');
+    }
+    function toggleActive(e) {
+    	console.log(e);	
+      	if (e.propertyName.includes('flex')) {
+        	this.classList.toggle('open-active');
+      	}
+    }
+  directionCards.forEach(card=>card.addEventListener('click', toggleOpen));
+  directionCards.forEach(card=>card.addEventListener('transitionend', toggleActive));
 })
